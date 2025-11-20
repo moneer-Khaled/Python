d7e8160df29acf5b14f6e84a18a6113fe26b65ff
moneer-Khaled <156200343+moneer-Khaled@users.noreply.github.com>
2025-11-20 11:21:12 +0100

    Delete exercises2.py

--- exercises2.py
+++
@@ -1,6 +0,0 @@
-# Write a program that checks if a number is positive, negative, or zero.
-# Use a for loop to print numbers from 1 to 10.
-# Use a while loop to print numbers from 10 down to 1.
-# Write a program that prints all even numbers from 1 to 20 using range().
-# Ask the user for a number. If it’s greater than 100, print "Big number!", otherwise print "Small number!".
-print("Hello ")
