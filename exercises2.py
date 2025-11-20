30cfb3d78e82dddecd10a8123306cec856a4f059
Muneer Khaled <kmoneer25@@yourmailserver.com>
2025-11-20 02:14:09 +0100

    update

--- exercises2.py
+++ exercises2.py
@@ -3,4 +3,4 @@
 # Use a while loop to print numbers from 10 down to 1.
 # Write a program that prints all even numbers from 1 to 20 using range().
 # Ask the user for a number. If it’s greater than 100, print "Big number!", otherwise print "Small number!".
-print("Hello")
+print("Hello World")
