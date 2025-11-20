f879fac20eef585e66744fdb85510e9eda3bff00
Muneer Khaled <kmoneer25@@yourmailserver.com>
2025-11-20 02:24:41 +0100

    updated

---
+++ exercises3.py
@@ -0,0 +1,24 @@
+# 1. Multiplication
+
+# Create a function that gets a number as a parameter, and then prints the multiplication table for that number from 1 to 10. E.g., when the parameter is 12, the first line printed is “1 x 12 = 12” and the last line printed is “10 x 12 = 120.”
+
+
+
+# 2. Sum of two strings
+
+# Write a function that takes two strings as parameters. The function returns the number of characters that the strings have in common. Each character counts only once, e.g., the strings "bee" and "peer" only have one character in common (the letter “e”). You can consider capitals different from lowercase letters. Note: the function should return the number of characters that the strings have in common, and not print it. To test the function, you can print the result in your main program.
+
+
+
+# 3. Guessing a number
+
+# Write a guessing number function that holds a random number between 1 and 10 and get a parameter number. The return for that function will be :
+
+# "Too big" if the parameter number is bigger than the held number.
+
+# "Too small" if the parameter number is smaller than the held number.
+
+# "You are SUPER" if the parameter number is the same as the held number.
+
+# Enter the parameter number via the terminal with the help of the input method.
+
